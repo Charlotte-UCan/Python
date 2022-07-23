db1f1136b35ba4e4175f0fc6b1c1ff338c548aa1
Charlotte-UCan <70787146+Charlotte-UCan@users.noreply.github.com>
2022-07-23 15:37:40 +0900

    Update sumInput.py

--- sumInput.py
+++ sumInput.py
@@ -4,15 +4,15 @@ sum = int(x) + int(y)
 print(sum)
 
 
-or
-Mặc định, số thực (float) trong Python chỉ có độ chính xác với tối đa 15 chữ số ở phần thập phân. Chúng ta có thể sử dụng module decimal được cài đặt sẵn trong Python để xác định độ chính xác là bao nhiêu số thập phân
+#or
+#Mặc định, số thực (float) trong Python chỉ có độ chính xác với tối đa 15 chữ số ở phần thập phân. Chúng ta có thể sử dụng module decimal được cài đặt sẵn trong Python để xác định độ chính xác là bao nhiêu số thập phân
 
-sum = decimal.Decimal(x) + decimal.Decimal(y)
+#sum = decimal.Decimal(x) + decimal.Decimal(y)
 
-Để kiểm soát độ chính xác phần thập phân có bao nhiêu chữ số, chúng ta có thể sử dụng hàm getcontext() với thuộc tính prec. Ví dụ:
-decimal.getcontext().prec = 10
+#Để kiểm soát độ chính xác phần thập phân có bao nhiêu chữ số, chúng ta có thể sử dụng hàm getcontext() với thuộc tính prec. Ví dụ:
+#decimal.getcontext().prec = 10
 
----> full code
+#---> full code
 import decimal
 
 x = input('please input first number: ')
@@ -21,4 +21,4 @@ decimal.getcontext().prec = 6
 sum = decimal.Decimal(x) + decimal.Decimal(y)
 
 
---->result we get 5 number after a decimal point, 1 before a decimal point, totally is 6
+#--->result we get 5 number after a decimal point, 1 before a decimal point, totally is 6
